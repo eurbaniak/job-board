64ef66028f65767a8f658913d8e6800b4af428f0
Edwin <50717284+Adderall-dev@users.noreply.github.com>
2021-10-12 14:38:03 +0200

    delete unnecessary query and component

--- src/GraphQL/Queries.js
+++ src/GraphQL/Queries.js
@@ -1,13 +1,5 @@
 import { gql } from "@apollo/client";
 
-export const GET_JOB_NAMES = gql`
-  query {
-    jobs {
-      title
-    }
-  }
-`;
-
 export const GET_JOB_INFORMATION = gql`
   query {
     jobs {
